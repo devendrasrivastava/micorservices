eb13d4eb7af6b0d3313ca7babe66a1dd3501fcf3
Kumard <kumard@rbos.co.uk>
2023-01-03 16:50:53 +0530

    code added

---
+++ apigateway/src/main/java/in/stackroute/apigateway/ApigatewayApplication.java
@@ -0,0 +1,26 @@
+package in.stackroute.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ApigatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApigatewayApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator apiRoutes(RouteLocatorBuilder builder){
+		return builder.routes()
+				.route("contacts_route",route -> route.path("/api/v1/contacts/**").uri("lb://contacts-service"))
+				.route("users_route",route->route.path("/api/v1/users/**").uri("lb://user-profile-service"))
+				.build();
+	}
+
+}
